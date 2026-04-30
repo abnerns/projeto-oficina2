5a9ea1061416062404257de6c3ef4e9085feebe6
Cadu mazalli <carlosemdl@gmail.com>
2026-04-30 06:09:39 -0300

    Arquivos iniciais do Back-End e conexão

---
+++ back-end/src/database.js
@@ -0,0 +1,7 @@
+const postgres = require('postgres');
+const dotenv = require('dotenv').config();
+
+const connectionString = process.env.DB_URL
+const sql = postgres(connectionString)
+
+module.exports = sql
